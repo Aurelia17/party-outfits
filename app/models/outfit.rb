255d6b52a69e0c013ed51bc800e7daadab607c3c
aurelia ritter <aureliaritter@gmail.com>
2023-05-21 10:03:41 +0200

    création du outfits controller avec index show new create et views de outfit _form et index

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,3 +1,10 @@
 class Outfit < ApplicationRecord
+  CATEGORIES = %w[WEDDING FORMAL-OUTFIT PARTY-COSTUME].freeze
   belongs_to :user
+  has_one_attached :photo
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :size, presence: true
+  validates :price_per_day, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'outfits/index'
+  get 'outfits/show'
+  get 'outfits/new'
+  get 'outfits/create'
+  get 'outfits/edit'
+  get 'outfits/update'
+  get 'outfits/destroy'
   devise_for :users
   root to: "outfits#index"
   resources :outfits
