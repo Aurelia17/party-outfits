e2c38a3fc9ba1dae28b65b0561572a07514a873b
aurelia ritter <aureliaritter@gmail.com>
2023-05-22 18:38:00 +0200

    ajout admin/user et modif outfit policy et view(devise edit)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_21_150518) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_22_160315) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_21_150518) do
     t.string "first_name"
     t.string "last_name"
     t.boolean "is_owner", default: false, null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
