382daa37f8a2e3c602625cd6c6a08040d4065050
tejas boodoo <shiveshboodoo391@gmail.com>
2023-05-23 21:24:39 +0400

    front pour outfits#index

--- app/policies/outfit_policy.rb
+++ app/policies/outfit_policy.rb
@@ -9,13 +9,13 @@ class OutfitPolicy < ApplicationPolicy
   end
 
   def update?
-    record.user == user || user.admin == true
+    record.user == user || user.admin
     # record: the restaurant passed to the `authorize` method in controller
     # user: the `current_user` signed in with Devise
   end
 
   def destroy?
-    record.user == user || user.admin == true
+    record.user == user || user.admin
   end
 
   class Scope < Scope
