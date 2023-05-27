56f998dd6226ebf25c5567160869e30c6b26f626
aurelia ritter <aureliaritter@gmail.com>
2023-05-27 16:57:05 +0400

    navbar avatar changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name is_owner])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name is_owner])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name is_owner avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name is_owner avatar])
   end
 
   private
